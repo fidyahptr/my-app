faa48d47e5147a7837a96d94bb81e4e2680cbeb8
fidyahptr <fidyahputri15@gmail.com>
2022-03-30 15:45:41 +0700

    add homework 3-2
    spotify

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import data from './Data';
 import Song from './components/song/index';
+import SearchBar from './pages/SearchBar/index';
 
 function App() {
-	const listSong = data.map(a => (
-		<Song
-			title={a.name}
-			artist={a.artists[0].name}
-			album={a.album.name}
-			image={a.album.images[1].url}
-			url={a.external_urls.spotify}
-		/>
-	));
 	return (
 		<div className="App">
 			<h1> Song </h1>
-			<div className="content">{listSong}</div>
+			<SearchBar />
 		</div>
 	);
 }
