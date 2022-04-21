d97771ca998c4293047c2b0c3501eedab3c5d940
fidyahptr <fidyahputri15@gmail.com>
2022-04-22 02:13:13 +0700

    add UI Library
    add landing and notFound page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import store from './store';
+import setToken from './redux/token/tokenAction';
 import CreatePlaylist from './pages/CreatePlaylist/index';
+import Landing from './pages/Landing/index';
+import NotFound from './pages/NotFound/index';
 
 function App() {
+	const token = useSelector(state => state.setToken);
 	return (
-		<Provider store={store}>
-			<div className="App">
-				<h1> Song </h1>
-				<CreatePlaylist />
-			</div>
-		</Provider>
+		// <ChakraProvider>
+		// 	<Provider store={store}>
+		<div className="App">
+			{/* <h1> Song </h1> */}
+			{/* <CreatePlaylist /> */}
+			{/* <Landing /> */}
+			{/* <NotFound /> */}
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						{!token ? <Landing /> : <Redirect to="/create-playlist" />}
+					</Route>
+					<Route path="/create-playlist">
+						{!token && <Redirect to="/" />}
+						<CreatePlaylist />
+					</Route>
+					<Route path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+		// 	</Provider>
+		// </ChakraProvider>
 	);
 }
 
--- my-app/src/components/formPlaylist/index.js
+++ my-app/src/components/formPlaylist/index.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Heading, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input } from '@chakra-ui/react';
+import { Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
+import { Flex, Box, Checkbox, Stack, Link, Button, Text, useColorModeValue } from '@chakra-ui/react';
 
 const FormPlaylist = ({ token, spotifyId, uris }) => {
 	const initialValue = {
@@ -8,6 +11,8 @@ const FormPlaylist = ({ token, spotifyId, uris }) => {
 	};
 	const [userId, setUserId] = useState('');
 	const [form, setForm] = useState(initialValue);
+
+	const isError = form.title.length >= 10;
 
 	const handleSubmit = async event => {
 		event.preventDefault();
@@ -55,7 +60,13 @@ const FormPlaylist = ({ token, spotifyId, uris }) => {
 			)
 			.then(response => {
 				console.log(response.data);
-				alert('berhasil');
+				const alertSucces = (
+					<Alert status="success">
+						<AlertIcon />
+						{form.title} Playlist added. Fire on!
+					</Alert>
+				);
+				console.log(alertSucces);
 			})
 			.catch(error => {
 				console.log(error);
@@ -74,8 +85,61 @@ const FormPlaylist = ({ token, spotifyId, uris }) => {
 
 	return (
 		<div>
-			<h1>Create Playlist</h1>
+			<Heading as="h2" size="xl" mt={6} mb={2}>
+				Create Playlist
+			</Heading>
+			<Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
+				<Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
+					<Stack spacing={4}>
+						<FormControl id="email">
+							<FormLabel>Email address</FormLabel>
+							<Input type="email" />
+						</FormControl>
+						<FormControl id="password">
+							<FormLabel>Password</FormLabel>
+							<Input type="password" />
+						</FormControl>
+						<Stack spacing={10}>
+							<Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
+								<Checkbox>Remember me</Checkbox>
+								<Link color={'blue.400'}>Forgot password?</Link>
+							</Stack>
+							<Button
+								bg={'blue.400'}
+								color={'white'}
+								_hover={{
+									bg: 'blue.500',
+								}}
+							>
+								Sign in
+							</Button>
+						</Stack>
+					</Stack>
+				</Box>
+			</Flex>
+
 			<form onSubmit={handleSubmit}>
+				{/* <FormControl isInvalid={isError}>
+					<FormLabel htmlFor="title">Email</FormLabel>
+					<Input
+						id="title"
+						type="text"
+						value={form.title}
+						onChange={handleChange}
+						maxLength={10}
+						colorScheme={'white'}
+					/>
+					{!isError ? (
+						<FormHelperText>Enter the email you&apos;d like to receive the newsletter on.</FormHelperText>
+					) : (
+						<FormErrorMessage>Email is required.</FormErrorMessage>
+					)}
+				</FormControl> */}
+				{/* {!isError ? (
+					<FormHelperText>Enter the email you&apos;d like to receive the newsletter on.</FormHelperText>
+				) : (
+					<FormErrorMessage>Email is required.</FormErrorMessage>
+				)} */}
 				<div>
 					<label htmlFor="title">Title</label>
 					<input
